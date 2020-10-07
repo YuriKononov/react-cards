90695a5d2ab50c5eb39ac569627abeb716c4b8d6
Yuri <yura.kononov.box@gmail.com>
2020-10-07 17:20:17 +0300

    redundant code is removed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,36 +1,12 @@
 import React ,{useState, useEffect} from 'react';
-import {BrouserRouter, Route} from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import axios from "axios";
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 
 import CardItem from './cardItem'
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -67,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Album() {
   const classes = useStyles();
   const [users, setUsers] = useState([])
-  const axios = require('axios');
+  
 
   const getUsers = async () => {
     try {
@@ -81,7 +57,6 @@ export default function Album() {
   }
   useEffect(() => {
     getUsers();
-    console.log('sssssss')
   },[])
   
 
--- src/components/cardItem.js
+++ src/components/cardItem.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Link } from 'react-router-dom'
-import Menu from '@material-ui/core/Menu';
 import Popper from '@material-ui/core/Popper';
 import Grow from '@material-ui/core/Grow';
 import Paper from '@material-ui/core/Paper';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
-
-
 
 
 const useStyles = makeStyles((theme) => ({
